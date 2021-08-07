35a6bcb4d929f052e514592c950d6de35262b54b
Chirag-Khatri-CK <kchiragkhatro2001@gmail.com>
2021-08-07 14:28:34 +0530

    first commit

---
+++ src/components/List.vue
@@ -0,0 +1,53 @@
+<template lang="en">
+    <div class="complete">
+    <h1>List</h1> 
+    <ul class="item" v-for="item in list" :key="item.id">
+    <li>{{item.id}}</li>
+    <li>{{item.title}}</li> 
+    <li>{{item.body}}</li>
+    </ul>
+    </div>
+</template>
+<script>
+
+import axios from 'axios';
+export default {
+    name: "List",
+    data()
+    {
+            return {
+                list:[]
+            }
+    },
+
+    async mounted()
+
+    {
+        let result = await axios.get("https://jsonplaceholder.typicode.com/posts");
+        console.warn(result.data)
+        this.list=result.data
+    }
+    
+};
+</script>
+
+<style>
+
+.complete{
+   background: linear-gradient(to right, #3399ff 0%, #00ffff 100%);
+   font-family: "Times New Roman", Times, serif;
+   font-size:20px;
+   float:left;
+}
+
+.complete h1{
+    border-bottom: 2px solid black; 
+    margin-left: 15px;
+    width: 70px;
+}
+
+.item u li{
+  list-style-type: circle;
+}
+
+</style>
